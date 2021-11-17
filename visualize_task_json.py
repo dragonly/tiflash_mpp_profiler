330aca4971e15005a183254d41e340bfde6326d6
Yilong Li <liyilongko@gmail.com>
2021-11-17 13:11:20 +0800

    json

---
+++ visualize_task_json.py
@@ -0,0 +1,59 @@
+import json
+import graphviz
+
+OUTPUT_DIR = 'output'
+
+
+def read_json(filename):
+    with open(filename, 'r') as fd:
+        data = json.load(fd)
+    return data
+
+
+class Graph:
+    def __init__(self):
+        self._g = graphviz.Digraph(comment="main graph")
+
+    def addTaskGraph(self, task_graph):
+        self._g.subgraph(task_graph._g)
+
+    def render(self):
+        path = '{}/query_task'.format(OUTPUT_DIR)
+        self._g.render(path, format='png')
+
+
+class TaskGraph:
+    def __init__(self, task_id, details):
+        self.serial = 0
+        self._g = graphviz.Digraph(name="cluster_"+str(task_id), comment="task graph")
+        self.details = details
+
+    def draw_executors(self, node):
+        self.draw_executor_nodes(node)
+        self.draw_executor_edges(node)
+
+    def draw_executor_nodes(self, node):
+        detail = self.details[node['id']]
+        label = '{}({})'.format(detail['type'], detail['id'])
+        self._g.node(node['id'], label)
+        for child in node['children']:
+            self.draw_executor_nodes(child)
+
+    def draw_executor_edges(self, node):
+        for child in node['children']:
+            self._g.edge(child['id'], node['id'])
+            self.draw_executor_edges(child)
+
+if __name__ == '__main__':
+    data = read_json('tracing.json')
+    task = data[0]
+    task_id = task['task_id']
+    root = task['executors']['structure']
+    details = task['executors']['details']
+    print(root)
+    print(details)
+    task_graph = TaskGraph(task_id, details)
+    task_graph.draw_executors(root)
+    graph = Graph()
+    graph.addTaskGraph(task_graph)
+    graph.render()
