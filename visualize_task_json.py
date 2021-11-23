477b1fd4016642c5760afa576206e8f7c9835451
Yilong Li <liyilongko@gmail.com>
2021-11-23 16:18:05 +0800

    draw task reference between sender&receiver

--- visualize_task_json.py
+++ visualize_task_json.py
@@ -1,5 +1,5 @@
 import json
-from typing import Dict
+from typing import Any, Dict, List
 
 import graphviz
 
@@ -12,47 +12,94 @@ def read_json(filename):
     return data
 
 
-class Graph:
-    def __init__(self):
-        self._g = graphviz.Digraph(comment='main graph')
-
-    def addTaskGraph(self, task_graph):
-        self._g.subgraph(task_graph._g)
-
-    def render(self):
-        path = '{}/query_task.dot'.format(OUTPUT_DIR)
-        self._g.render(path, format='png')
-
-
 class TaskGraph:
     def __init__(self, task):
-        self.serial = 0
+        self._task = task
         self._g = graphviz.Digraph(
             name='cluster_'+str(task['task_id']), comment='task graph')
         self._g.attr(label=_gen_label_task(task), labeljust='l')
-        self.details = task['executors']['details']
+        self._details: Dict[str, Any] = task['executors']['details']
+        self._task_id = task['task_id']
+        self._downstream_senders: Dict[str, List[str]] = {}
+
+    @property
+    def g(self):
+        return self._g
+
+    @property
+    def id(self):
+        return self._task_id
+
+    @property
+    def sender_executor_id(self):
+        if 'sender_executor_id' in self._task:
+            return self._task['sender_executor_id']
+        return None
+
+    @property
+    def sender_executor(self):
+        sender_executor_id = self._task['sender_executor_id']
+        return self._details[sender_executor_id]
+
+    @property
+    def downstream_senders(self):
+        return self._downstream_senders
 
     def draw_executors(self):
-        root = task['executors']['structure']
+        root = self._task['executors']['structure']
         self._draw_executor_nodes(root)
         self._draw_executor_edges(root)
+        self._collect_downstreams()
+
+    def _collect_downstreams(self):
+        for executor_id, detail in self._details.items():
+            if 'downstream_task_ids' in detail:
+                self._downstream_senders[executor_id] = detail['downstream_task_ids']
 
     def _draw_executor_nodes(self, node):
-        detail = self.details[node['id']]
-        label = '{}({})\l{}'.format(
-            detail['type'], detail['id'], _gen_label_executor(detail))
-        self._g.node(node['id'], label, shape='box')
+        detail = self._details[node['id']]
+        label = '{}\l{}'.format(detail['type'], _gen_label_executor(detail))
+        self._g.node(self.get_node_id(node['id']), label, shape='box')
         for child in node['children']:
             self._draw_executor_nodes(child)
 
     def _draw_executor_edges(self, node):
         for child in node['children']:
-            self._g.edge(child['id'], node['id'])
+            self._g.edge(self.get_node_id(child['id']), self.get_node_id(node['id']))
             self._draw_executor_edges(child)
+
+    def get_node_id(self, node_id):
+        return '{}-{}'.format(self._task_id, node_id)
+
+
+class Graph:
+    def __init__(self):
+        self._g = graphviz.Digraph(comment='main graph')
+        self._task_graphs: Dict[str, TaskGraph] = {}
+
+    def addTaskGraph(self, task_graph: TaskGraph):
+        self._g.subgraph(task_graph.g)
+        self._task_graphs[task_graph.id] = task_graph
+
+    # draw executor references corssing task boundaries
+    def _draw_task_references(self):
+        for _, receiver_task_graph in self._task_graphs.items():
+            for receiver_executor_id, sender_task_ids in receiver_task_graph.downstream_senders.items():
+                receiver_node_id = receiver_task_graph.get_node_id(receiver_executor_id)
+                for sender_task_id in sender_task_ids:
+                    sender_task_graph = self._task_graphs[sender_task_id]
+                    sender_executor_id = sender_task_graph.sender_executor_id
+                    sender_node_id = sender_task_graph.get_node_id(sender_executor_id)
+                    self._g.edge(sender_node_id, receiver_node_id)
+
+    def render(self):
+        path = '{}/query_task.dot'.format(OUTPUT_DIR)
+        self._draw_task_references()
+        self._g.render(path, format='png')
 
 
 def _gen_label_executor(detail):
-    not_keys = {'id', 'type'}
+    not_keys = {'type', }
     return _gen_label(detail, not_keys)
 
 
